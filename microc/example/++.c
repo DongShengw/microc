1a9682e8fecae470ecd8abd9529bdaf8239803a5
yyx0111 <749857410@qq.com>
2022-06-05 08:55:17 +0800

    测试

---
+++ microc/example/++.c
@@ -0,0 +1,9 @@
+void main(int n) {
+
+   print n;
+   print n++;
+   print n;
+
+   print ++n;
+   print n;
+}
---
+++ microc/example/break.c
@@ -0,0 +1,10 @@
+int main(){
+    int i=0;
+    int n=1;
+    print(i);
+    switch(n){
+        case 1:{i=n+n;break;}
+        case 5:i=i+n*n;
+    }
+    print(i);
+}
